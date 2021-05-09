a0de02b78130f640de9642ef64c01e5b5020ee12
Wei Zhang <wei@novvum.io>
2021-05-08 23:21:47 -0700

    timer is displaying and working properly

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,11 +1,62 @@
-/** @jsx jsx */
-import { jsx, Flex } from 'theme-ui'
+/** @jsxImportSource theme-ui */
 
-const Timer = () => {
-    return (
-    <Flex>
+import React, { useState } from "react";
+import { Flex, Paragraph, Button } from "theme-ui";
+import useCountdown from "../hooks/useCountdown";
+import styled from "styled-components";
 
-    </Flex>)
-}
+const RowFlexBox = styled(Flex)`
+  display: flex;
+  flex-direction: row;
+`;
 
-export default Timer 
+const ColumnFlexBox = styled(Flex)`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Timer = (props) => {
+  const {
+    onStart,
+    onPause,
+    onResume,
+    onReset,
+    getTime,
+    getHour,
+    getMinute,
+    getSecond,
+  } = useCountdown(parseInt(props.startTime));
+
+  const { timerRunning, setTimerRunning } = useState(false);
+
+  return (
+    <>
+      <RowFlexBox
+        sx={{
+          textAlign: "center",
+        }}
+      >
+        <ColumnFlexBox>
+          <Paragraph>{getHour()}</Paragraph>
+          <Paragraph>H</Paragraph>
+        </ColumnFlexBox>
+        <ColumnFlexBox>
+          <Paragraph>{getMinute()}</Paragraph>
+          <Paragraph>M</Paragraph>
+        </ColumnFlexBox>
+        <ColumnFlexBox>
+          <Paragraph>{getSecond()}</Paragraph>
+          <Paragraph>S</Paragraph>
+        </ColumnFlexBox>
+      </RowFlexBox>
+      <RowFlexBox>
+        <Button onClick={onStart}>Start</Button>
+        <Button onClick={onPause}>Pause</Button>
+        <Button onClick={onResume}>Resume</Button>
+        <Button onClick={onReset}> Reset</Button>
+      </RowFlexBox>
+    </>
+  );
+};
+
+export default Timer;
--- src/hooks/useCountdown.js
+++ src/hooks/useCountdown.js
@@ -1,42 +1,101 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
+const useCountdown = (startTime) => {
+  //helper conversion functions for time
 
-const useCountdown = ({startTime = 6000 }) => {   
-    let initialTime = startTime
+  const calculateHour = (sec) => {
+    return Math.floor(sec / (60 * 60));
+  };
 
-    const [countingDown, setCountingDown] = useState(false)
+  const calculateMinute = (sec) => {
+    return Math.floor((sec % (60 * 60)) / 60);
+  };
 
-    useEffect(() => {
-        while(countingDown){
-            initialTime--
+  const calculateSecond = (sec) => {
+    return sec % 60;
+  };
+
+  const [hour, setHour] = useState(calculateHour(startTime));
+  const [minute, setMinute] = useState(calculateMinute(startTime));
+  const [second, setSecond] = useState(calculateSecond(startTime));
+  const [countingDown, setCountingDown] = useState(false);
+
+  useEffect(() => {
+    if (countingDown) {
+      const timer = setTimeout(() => {
+        if (second > 0) {
+          setSecond(second - 1);
         }
-    }, [countingDown])
 
-    // start timer
-   const onStart = () => {
-        setCountingDown(true)
+        if (second === 0) {
+          setSecond(60);
+
+          if (minute >= 1) {
+            setMinute(minute - 1);
+          }
+
+          if (minute === 0 && hour >= 1) {
+            setHour(hour - 1);
+          }
+        }
+      }, 1000);
+      return () => clearTimeout(timer);
     }
+  });
 
+  // start timer
+  const onStart = () => {
+    setCountingDown(true);
+  };
 
-    // pause timer
-    const onPause = () => {
-        setCountingDown(false)
-    }
+  // pause timer
+  const onPause = () => {
+    setCountingDown(false);
+  };
 
-    // resume timer
-    const onResume = () => {
-        setCountingDown(true)
-    }
+  // resume timer
+  const onResume = () => {
+    setCountingDown(true);
+  };
 
-    // reset timer
-    const onReset = () => {
-        initialTime = startTime
-    }
+  // reset timer
+  const onReset = () => {
+    setHour(calculateHour(startTime));
+    setMinute(calculateMinute(startTime));
+    setSecond(calculateSecond(startTime));
+  };
 
-    //format the time to return 
-    const formatTime = () => {
-        
-    }
+  //format the time to return
+  const getTime = () => {
+    return {
+      h: hour,
+      m: minute,
+      s: second,
+    };
+  };
 
+  const getHour = () => {
+    return hour;
+  };
 
-}
+  const getMinute = () => {
+    return minute;
+  };
+
+  const getSecond = () => {
+    return second;
+  };
+
+  return {
+    onStart,
+    onPause,
+    onResume,
+    onReset,
+    getTime,
+    getHour,
+    getMinute,
+    getSecond,
+  };
+};
+
+export default useCountdown;
